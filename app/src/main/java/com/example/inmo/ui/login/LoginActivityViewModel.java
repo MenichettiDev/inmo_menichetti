d8809e42a82d9259ab200159b14b2f22c83e2a3f
MenichettiDev <menichetti.dev@gmail.com>
2025-10-07 17:29:53 -0300

    Actualiza lógica de LoginActivityViewModel

--- app/src/main/java/com/example/inmo/ui/login/LoginActivityViewModel.java
+++ app/src/main/java/com/example/inmo/ui/login/LoginActivityViewModel.java
@@ -24,7 +24,7 @@ public class LoginActivityViewModel extends AndroidViewModel {
     public LoginActivityViewModel(@NonNull Application application) {
         super(application);
         context = getApplication();
-        
+
     }
 
 
